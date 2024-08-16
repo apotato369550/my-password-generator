10533ebafff6d36de3a612a0359b64e7498946af
apotato369550 <65766356+apotato369550@users.noreply.github.com>
2024-08-16 20:58:59 +0800

    Add: Added header link to portfolio site

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 import PasswordGenerator from './components/PasswordGenerator'
 
 function App() {
   return (
     <>
+      <Header />
       <PasswordGenerator />
     </>
   )
